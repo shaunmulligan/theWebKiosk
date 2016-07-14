69b330d4a10bfcb6d6d524834cdfd48139c4a687
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-07-14 16:11:45 +0100

    add launch script

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
-chmod 777 /dev/vchiq
 
 cp /opt/vc/lib/* /usr/lib/arm-linux-gnueabihf/
 ldconfig
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host_run/dbus/system_bus_socket
+chmod 777 /dev/vchiq
+groups pi
 su - pi -c /opt/oxide/start.sh
