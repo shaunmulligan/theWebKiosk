161820fe3ebe500e43114613e4b7bc9bd4763caf
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-07-15 12:13:07 +0100

    change perms of chrome-sandbox

--- launch.sh
+++ launch.sh
@@ -3,5 +3,7 @@ cp /opt/vc/lib/* /usr/lib/arm-linux-gnueabihf/
 ldconfig
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host_run/dbus/system_bus_socket
 chmod 777 /dev/vchiq
+chown root:pi /opt/oxide/usr/lib/arm-linux-gnueabihf/oxide-qt/chrome-sandbox
+chmod 4755 /opt/oxide/usr/lib/arm-linux-gnueabihf/oxide-qt/chrome-sandbox
 groups pi
 su - pi -c /opt/oxide/start.sh
